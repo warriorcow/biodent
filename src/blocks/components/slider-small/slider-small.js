e6d00c89a10306d40975b08d5ef5e8706a4c569c
Ilya Borisov <battersman27@gmail.com>
2024-04-04 01:34:43 +0700

    Start creating services page

--- src/blocks/components/slider-small/slider-small.js
+++ src/blocks/components/slider-small/slider-small.js
@@ -3,6 +3,9 @@ import 'swiper/scss';
 import 'swiper/scss/pagination';
 import { Navigation } from 'swiper/modules';
 
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
 new Swiper('.slider-small', {
     modules: [Navigation],
     slidesPerView: 6,
@@ -12,3 +15,5 @@ new Swiper('.slider-small', {
         prevEl: '.slider-small__arrow-prev'
     }
 });
+
+Fancybox.bind('[data-fancybox="certificates"]');
